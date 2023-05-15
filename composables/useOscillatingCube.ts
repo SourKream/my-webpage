aa45dd9b92cb91beea83311f25fe4d90f2bb1b4a
Shantanu Kumar <shantanu@productiv.ai>
2023-05-16 02:18:42 +0530

    Fix p5

---
+++ composables/useOscillatingCube.ts
@@ -0,0 +1,44 @@
+function render (p5: any) {
+  const width = 700
+  const height = 480
+
+  const size = 20
+  const num = 16
+  const border = 6
+
+  let time = 0
+
+  p5.setup = () => {
+    const canvas = p5.createCanvas(width, height, p5.WEBGL)
+    canvas.parent('cube_holder')
+    p5.ortho()
+  }
+
+  p5.draw = () => {
+    p5.background(255)
+    p5.fill(150)
+    p5.rotateX(-p5.asin(p5.tan(p5.PI / 6)))
+    p5.rotateY(p5.PI / 4)
+    p5.orbitControl()
+
+    for (let z = 0; z < num; z++) {
+      for (let x = 0; x < num; x++) {
+        p5.push()
+        const r = p5.dist((x - num / 2) * size + size / 2, (z - num / 2) * size + size / 2, 0, 0)
+        const h = p5.floor(140 + 80 * p5.cos(0.027 * r - time))
+        p5.translate((x - num / 2) * size + size / 2, 0, (z - num / 2) * size + size / 2)
+        p5.normalMaterial()
+        p5.box(size - border, h, size - border)
+        p5.pop()
+      }
+    }
+
+    time += 0.075
+  }
+}
+
+export default function useOscillatingCube () {
+  return {
+    render
+  }
+}
--- pages/posts/2018/dragon-curve.vue
+++ pages/posts/2018/dragon-curve.vue
@@ -1,13 +1,24 @@
 <template>
-  <p>
-    This post is about how to build a dragon curve
-  </p>
+  <div>
+    <p>
+      This post is about how to build a dragon curve
+    </p>
+    <div id="cube_holder" />
+  </div>
 </template>
 
 <script setup>
 definePageMeta({
-  title: 'Dragon Curve',
-  date: 'Jan 01, 2018',
+  title: 'The Dragon Curve',
+  date: 'Jan 09, 2018',
   layout: 'post'
+})
+
+const { render } = useOscillatingCube()
+
+onMounted(async () => {
+  const { default: P5 } = await import('p5')
+  // eslint-disable-next-line no-new
+  new P5(render)
 })
 </script>
