ac7cfcf24926b35d4ef4b2e78066402647bd5ba1
Shantanu Kumar <shantanu@productiv.ai>
2023-05-15 23:21:37 +0530

    Adding eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,30 @@
 <template>
   <div>
-
     <div class="page-content">
       <div class="wrapper">
+        <center>
+          <h1 class="site-heading">
+            Shantanu
+          </h1>
 
-    <center>
+          <p class="text">
+            Hi. Rebuilding this app using Nuxt3!
+          </p>
+        </center>
 
-      <h1 class="site-heading">Shantanu Kumar</h1>
-    
-    <p class="text">
-      Hi. I am a software engineer from <a href="http://www.iitd.ac.in">IIT Delhi</a> living in California. <br>
-      I like to let my code draw on the internet sometimes.
-    </p>
+        <h1 class="page-heading">
+          Posts
+        </h1>
 
-  </center>
-
-  <h1 class="page-heading">Posts</h1>
-
-  <center>    <iframe src="https://editor.p5js.org/SourKream/embed/LS_VYq7dC" width=600 height=600 style="border:none;"></iframe>
-  </center>
-
-</div>
+        <center>
+          <iframe
+            src="https://editor.p5js.org/SourKream/embed/LS_VYq7dC"
+            width="600"
+            height="600"
+            style="border:none;"
+          />
+        </center>
+      </div>
     </div>
-
   </div>
 </template>
