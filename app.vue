f93957f067f3654e2f8840be18b8f68edd87ab9d
Shantanu Kumar <shantanu@productiv.ai>
2023-05-15 23:44:55 +0530

    Fix eslint

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="page-content">
+    <div class="wrapper">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,26 @@
 <template>
   <div>
-    <div class="page-content">
-      <div class="wrapper">
-        <center>
-          <h1 class="site-heading">
-            Shantanu
-          </h1>
+    <center>
+      <h1 class="site-heading">
+        Shantanu
+      </h1>
 
-          <p class="text">
-            Hi. Rebuilding this app using Nuxt3!
-          </p>
-        </center>
+      <p class="text">
+        Hi. Rebuilding this app using Nuxt3!
+      </p>
+    </center>
 
-        <h1 class="page-heading">
-          Posts
-        </h1>
+    <h1 class="page-heading">
+      Posts
+    </h1>
 
-        <center>
-          <iframe
-            src="https://editor.p5js.org/SourKream/embed/LS_VYq7dC"
-            width="600"
-            height="600"
-            style="border:none;"
-          />
-        </center>
-      </div>
-    </div>
+    <center>
+      <iframe
+        src="https://editor.p5js.org/SourKream/embed/LS_VYq7dC"
+        width="600"
+        height="600"
+        style="border:none;"
+      />
+    </center>
   </div>
 </template>
