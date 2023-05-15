158823c273fb6f5e90411e8c1a771951df26cbb1
Shantanu Kumar <shantanu@productiv.ai>
2023-05-15 23:07:18 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <div class="page-content">
+      <div class="wrapper">
+
+    <center>
+
+      <h1 class="site-heading">Shantanu Kumar</h1>
+    
+    <p class="text">
+      Hi. I am a software engineer from <a href="http://www.iitd.ac.in">IIT Delhi</a> living in California. <br>
+      I like to let my code draw on the internet sometimes.
+    </p>
+
+  </center>
+
+  <h1 class="page-heading">Posts</h1>
+
+  <center>    <iframe src="https://editor.p5js.org/SourKream/embed/LS_VYq7dC" width=600 height=600 style="border:none;"></iframe>
+  </center>
+
+</div>
+    </div>
+
+  </div>
+</template>
