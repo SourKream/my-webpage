94c3a0a0aba661afa9bd69946f297871218ddf7e
Shantanu Kumar <shantanu@productiv.ai>
2023-05-16 14:15:52 +0530

    Fix vscode eslint config

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div style="text-align: center">
-      <h1 class="site-heading">Welcome to my webpage</h1>
+      <h1 class="site-heading">Welcome to my webpage!</h1>
 
       <p class="text">
-        Hi. I am in the process of rebuilding this app using Nuxt3!
+        Hi. I am in the process of rebuilding this app using Nuxt 3!
       </p>
 
       <OscillatingCube />
